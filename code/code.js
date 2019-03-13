39e72d3336e9f2124447e45734e5feb7dd4c7e01
SesswireD <37369020+SesswireD@users.noreply.github.com>
2019-03-13 14:49:53 +0100

    Add files via upload

--- code/code.js
+++ code/code.js
@@ -1,7 +1,4 @@
-
-/// FIRST PART ADDING BASIC HTML/STYLE
-
-// run this function when the document is loaded
+// run the script when the document is loaded
 window.onload = function() 
 {
 
@@ -28,7 +25,7 @@ window.onload = function()
             this.isbn = isbn
             this.edition = edition;
         }
-    };
+    }
     
     class author
     {
@@ -53,34 +50,35 @@ window.onload = function()
 
     }
 
-	//dynamically create the elements and content for info.html page
+    
+    // PAGE SPECIFIC JAVASCRIPT CODE
     if (document.body.className == 'info')
     {
-    	 function createModdableElement(name)
-    	{
-    	    	e = document.createElement(name);
-        		e.setAttribute('class', 'moddable');
-        		return e;
-    	}
+         function createModdableElement(name)
+        {
+                e = document.createElement(name);
+                e.setAttribute('class', 'moddable');
+                return e;
+        }
 
-		//Add styleSheet
+        //Add styleSheet
         var head = document.head;
-		var sheet = document.createElement('link');
-		sheet.setAttribute('href', 'style/main.css');
-		sheet.setAttribute('rel', "stylesheet");
-		sheet.setAttribute('type', "text/css");
-		document.head.append(sheet)
+        var sheet = document.createElement('link');
+        sheet.setAttribute('href', 'style/main.css');
+        sheet.setAttribute('rel', "stylesheet");
+        sheet.setAttribute('type', "text/css");
+        document.head.append(sheet)
 
-		document.body.classList.add("home");
+        document.body.classList.add("home");
 
-    	// Add header
-    	var header = document.getElementsByTagName('header')[0];
-    	var heading = document.createElement("h1");
-    	var headingText = document.createTextNode("Game of Thrones");
-    	heading.appendChild(headingText);
-    	header.appendChild(heading);
+        // Add header
+        var header = document.getElementsByTagName('header')[0];
+        var heading = document.createElement("h1");
+        var headingText = document.createTextNode("Game of Thrones");
+        heading.appendChild(headingText);
+        header.appendChild(heading);
 
-    	//Add Article and sections
+        //Add Article and sections
         var gotPublisher = new publisher("Bantam Books", "New York city, United States", "1945");
         var gotAuthor = new author("George R. R. Martin", "20th of september 1948", "Bayonne, New Jersey, United States", "American");
         var gotSeries = new bookSeries("A Song of Ice and Fire", gotAuthor.name, "fantasy", gotPublisher.name, "English");
@@ -88,11 +86,11 @@ window.onload = function()
 
         //Add Article
         
-    	var article = document.getElementsByTagName('article')[0];
-    	var articleTitle = document.createElement('h2');
-    	articleTitle.textContent = "Info";
+        var article = document.getElementsByTagName('article')[0];
+        var articleTitle = document.createElement('h2');
+        articleTitle.textContent = "Info";
         article.appendChild(articleTitle);        
-    	article.setAttribute('class', 'text');
+        article.setAttribute('class', 'text');
         var articleText = document.createTextNode("This is a website about the bookseries " + gotSeries.title + ". This series now has a total of five books; the first book that came out was " + gotBook.title + ".");
         article.appendChild(articleText);
 
@@ -106,7 +104,7 @@ window.onload = function()
 
         //First paragraph
         var sectionP1 = document.createElement('p');
-    	var sectionP1Text = document.createTextNode("A Game of Thrones takes place over the course of one year on or near the fictional continent of Westeros. The story begins when King Robert visits the northern castle Winterfell to ask Ned Stark to be his right-hand assistant, or Hand of the King. The previous Hand, Jon Arryn, died under suspicious circumstances. Robert comes with his queen, Cersei Lannister, and his retinue, which includes a number of Lannisters. Just after the royal party arrives, Ned’s wife, Catelyn, receives a message claiming that the Lannister family was responsible for the death of the former Hand. ");
+        var sectionP1Text = document.createTextNode("A Game of Thrones takes place over the course of one year on or near the fictional continent of Westeros. The story begins when King Robert visits the northern castle Winterfell to ask Ned Stark to be his right-hand assistant, or Hand of the King. The previous Hand, Jon Arryn, died under suspicious circumstances. Robert comes with his queen, Cersei Lannister, and his retinue, which includes a number of Lannisters. Just after the royal party arrives, Ned’s wife, Catelyn, receives a message claiming that the Lannister family was responsible for the death of the former Hand. ");
         section.appendChild(sectionP1);
         sectionP1.appendChild(sectionP1Text);
         article.appendChild(section);
@@ -125,7 +123,7 @@ window.onload = function()
         var sectionP2Title = document.createElement('h3');
         sectionP2Title.textContent = "The Author";
         var sectionP2 = document.createElement('p');
-    	var sectionP2Text = document.createTextNode("The author of the bookseries " + gotSeries.title + " is " + gotAuthor); //". He was born in " + gotAuthor.placeOfBirth " in " + gotAuthor.dateOfBirth ".");
+        var sectionP2Text = document.createTextNode("The author of the bookseries " + gotSeries.title + " is " + gotAuthor); //". He was born in " + gotAuthor.placeOfBirth " in " + gotAuthor.dateOfBirth ".");
         section.appendChild(sectionP2Title);
         section.appendChild(sectionP2);
         sectionP2.appendChild(sectionP2Text);
@@ -142,20 +140,23 @@ window.onload = function()
         section.appendChild(sectionP2);
         sectionP2.appendChild(sectionP2Text);
         
-    	//Add footer
-    	var footer =  document.getElementsByTagName('footer')[0];
-    	var footerImg = document.createElement('img');
-    	var footerText = document.createTextNode("Created by: Daniël Bezema, Bram Kreuger en Nikki Evers. \n University Utrecht \n Webtechnology \n INFOB2WT ");
-    	footerImg.setAttribute('src', "style/images/utrecht_university_english_logo_transparent.png");
-    	footerImg.setAttribute('alt', 'Image of Utrecht university logo');
-    	footer.appendChild(footerText);
-    	footer.appendChild(footerImg);
+        //Add footer
+        var footer =  document.getElementsByTagName('footer')[0];
+        var footerImg = document.createElement('img');
+        var footerText = document.createTextNode("Created by: Daniël Bezema, Bram Kreuger en Nikki Evers. \n University Utrecht \n Webtechnology \n INFOB2WT ");
+        footerImg.setAttribute('src', "style/images/utrecht_university_english_logo_transparent.png");
+        footerImg.setAttribute('alt', 'Image of Utrecht university logo');
+        footer.appendChild(footerText);
+        footer.appendChild(footerImg);
 
-    	
+        
 
-	} 
+    } 
 
-	//dynamically add the navigation bar for all pages
+
+    // PAGE GENERAL JAVASCRIPT CODE
+
+    //dynamically add the navigation bar for all pages
     var nav = document.getElementsByTagName('nav')[0];
     var links = ["Home", "Plot", "Characters", "World", "Author", "Info"];
     for(i=0; i < links.length; i++) 
@@ -174,156 +175,162 @@ window.onload = function()
         }
     }
     
-    // Get necessary elements for a menu
-    var header = document.getElementsByTagName('header')[0];
-    var menu = document.createElement('div');
-    var label = document.createElement('label');
-    var labelText = document.createTextNode('Modify appearance for this site:');
 
-
-    menu.setAttribute('id','menu');
-    label.setAttribute('for', 'menu');
-    header.appendChild(menu);
-    menu.appendChild(labelText);
-
-	
-
-    // add select for modifiable elements 
-    var selector1 = document.createElement('select');
-    var optgroup1 = document.createElement('optgroup');
-    optgroup1.setAttribute('label', 'Element')
-    menu.append(selector1);
-    selector1.appendChild(optgroup1);
-    selector1.onchange = function() {changeSelector1()};
-    
-    // find all existing modifiable elements on current page    	
-    var semElements = document.querySelectorAll('body,nav,header,footer,aside,article,section');
-
-    for(i=0; i < semElements.length; i++)
+    // check if window has mobile dimensions
+    // only create selects for non-mobile
+    if ( window.outerWidth > 420) 
     {
-        var z = document.createElement("option");
-        z.setAttribute("value", i);
-        var t = document.createTextNode(semElements[i].localName);
-        z.appendChild(t);
-        selector1.appendChild(z);            
-    }  
+       // Get necessary elements for a menu
+        var header = document.getElementsByTagName('header')[0];
+        var menu = document.createElement('div');
+        var label = document.createElement('label');
+        var labelText = document.createTextNode('Modify appearance for this site:');
 
-    // add select for fontsize
-    var selector2 = document.createElement('select');
-    var optgroup2 = document.createElement('optgroup');
 
-    optgroup2.setAttribute('label', 'Fontsize')
-    menu.appendChild(selector2);
-    selector2.appendChild(optgroup2);
-    selector2.onchange = function() {changeSelector2()};
+        menu.setAttribute('id','menu');
+        label.setAttribute('for', 'menu');
+        header.appendChild(menu);
+        menu.appendChild(labelText);
 
-    var selectedFontSize = 30;
-    var fontList = ["xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large"];
-    for (i=0; i< fontList.length;i++) 
-    {
-        var o = document.createElement('option');
-        o.setAttribute("value", fontList[i]);
-        var t = document.createTextNode(fontList[i]);
-        o.appendChild(t);
-        optgroup2.appendChild(o);
-
-    }
-
-    // add select for color
-    var selector3 = document.createElement('select');
-    var optgroup3 = document.createElement('optgroup');
-
-    optgroup3.setAttribute('label', 'Color')
-    menu.appendChild(selector3);
-    selector3.appendChild(optgroup3);
-    selector3.onchange = function() {changeSelector3()};
-
-    var selectedColor;
-    var colorList = ['blue','red','green','white'];
-    for (i=0; i< colorList.length;i++) 
-    {
-        var o = document.createElement('option');
-        o.setAttribute("value", colorList[i]);
-        var t = document.createTextNode(colorList[i]);
-        o.appendChild(t);
-        optgroup3.appendChild(o);
-
-    }
-    // add select for background
-    var selector4 = document.createElement('select');
-    var optgroup4 = document.createElement('optgroup');
-
-    optgroup4.setAttribute('label', 'Backcolor')
-    menu.appendChild(selector4);
-    selector4.appendChild(optgroup4);
-    selector4.onchange = function() {changeSelector4()};
-
-    var selectedBackColor;
-    var backgroundList = ['blue','red','green','black'];
-    for (i=0; i< backgroundList.length;i++) 
-    {
-        var o = document.createElement('option');
-        o.setAttribute("value", backgroundList[i]);
-        var t = document.createTextNode(backgroundList[i]);
-        o.appendChild(t);
-        optgroup4.appendChild(o);
-
-    }
-
-    var styleButton = document.createElement("button");
-    styleButton.textContent = "Change style";
-    styleButton.setAttribute("id", "selectorButton");
-    styleButton.onclick = function() {styleChange()};
-    menu.appendChild(styleButton);
-
-    var selectedElementID = 0;
-    function changeSelector1()
-    {
-        selectedElementID = selector1.value;
-    }
-
-    var selectedFontSize = "medium";
-    function changeSelector2()
-    {
-        selectedFontSize = selector2.value;
-    }
-    var selectedColor = "blue";
-    function changeSelector3()
-    {
-        selectedColor = selector3.value;
-    }
-
-    var selectedBackColor = "blue";
-    function changeSelector4()
-    {
-        selectedBackColor = selector4.value;
-    }
-
-    function styleChange()
-    {       
-        e = semElements[selectedElementID];          
-        e.style.color = selectedColor;
-        e.style.backgroundColor = selectedBackColor;
-        e.style.fontSize = selectedFontSize;
         
-        childeren = semElements[selectedElementID].children
-        for(i=0; i < childeren.length; i++)
-        {
-            childeren[i].style.color = selectedColor;
-            childeren[i].style.fontSize = selectedFontSize;
-            childeren[i].style.backgroundColor = selectedBackColor;
-        }   
 
-        if(e.tagName == "BODY")
+        // add select for modifiable elements 
+        var selector1 = document.createElement('select');
+        var optgroup1 = document.createElement('optgroup');
+        optgroup1.setAttribute('label', 'Element')
+        menu.append(selector1);
+        selector1.appendChild(optgroup1);
+        selector1.onchange = function() {changeSelector1()};
+        
+        // find all existing modifiable elements on current page        
+        var semElements = document.querySelectorAll('body,nav,header,footer,aside,article,section');
+
+        for(i=0; i < semElements.length; i++)
         {
-            e.classList.remove(e.className);
-        }        
+            var z = document.createElement("option");
+            z.setAttribute("value", i);
+            var t = document.createTextNode(semElements[i].localName);
+            z.appendChild(t);
+            selector1.appendChild(z);            
+        }  
+
+        // add select for fontsize
+        var selector2 = document.createElement('select');
+        var optgroup2 = document.createElement('optgroup');
+
+        optgroup2.setAttribute('label', 'Fontsize')
+        menu.appendChild(selector2);
+        selector2.appendChild(optgroup2);
+        selector2.onchange = function() {changeSelector2()};
+
+        var selectedFontSize = 30;
+        var fontList = ["xx-small", "x-small", "small", "medium", "large", "x-large", "xx-large"];
+        for (i=0; i< fontList.length;i++) 
+        {
+            var o = document.createElement('option');
+            o.setAttribute("value", fontList[i]);
+            var t = document.createTextNode(fontList[i]);
+            o.appendChild(t);
+            optgroup2.appendChild(o);
+
+        }
+
+        // add select for color
+        var selector3 = document.createElement('select');
+        var optgroup3 = document.createElement('optgroup');
+
+        optgroup3.setAttribute('label', 'Color')
+        menu.appendChild(selector3);
+        selector3.appendChild(optgroup3);
+        selector3.onchange = function() {changeSelector3()};
+
+        var selectedColor;
+        var colorList = ['blue','red','green','white'];
+        for (i=0; i< colorList.length;i++) 
+        {
+            var o = document.createElement('option');
+            o.setAttribute("value", colorList[i]);
+            var t = document.createTextNode(colorList[i]);
+            o.appendChild(t);
+            optgroup3.appendChild(o);
+
+        }
+        // add select for background
+        var selector4 = document.createElement('select');
+        var optgroup4 = document.createElement('optgroup');
+
+        optgroup4.setAttribute('label', 'Backcolor')
+        menu.appendChild(selector4);
+        selector4.appendChild(optgroup4);
+        selector4.onchange = function() {changeSelector4()};
+
+        var selectedBackColor;
+        var backgroundList = ['blue','red','green','black'];
+        for (i=0; i< backgroundList.length;i++) 
+        {
+            var o = document.createElement('option');
+            o.setAttribute("value", backgroundList[i]);
+            var t = document.createTextNode(backgroundList[i]);
+            o.appendChild(t);
+            optgroup4.appendChild(o);
+
+        }
+
+        var styleButton = document.createElement("button");
+        styleButton.textContent = "Change style";
+        styleButton.setAttribute("id", "selectorButton");
+        styleButton.onclick = function() {styleChange()};
+        menu.appendChild(styleButton);
+
+        var selectedElementID = 0;
+        function changeSelector1()
+        {
+            selectedElementID = selector1.value;
+        }
+
+        var selectedFontSize = "medium";
+        function changeSelector2()
+        {
+            selectedFontSize = selector2.value;
+        }
+        var selectedColor = "blue";
+        function changeSelector3()
+        {
+            selectedColor = selector3.value;
+        }
+
+        var selectedBackColor = "blue";
+        function changeSelector4()
+        {
+            selectedBackColor = selector4.value;
+        }
+
+        function styleChange()
+        {       
+            e = semElements[selectedElementID];          
+            e.style.color = selectedColor;
+            e.style.backgroundColor = selectedBackColor;
+            e.style.fontSize = selectedFontSize;
+            
+            childeren = semElements[selectedElementID].children
+            for(i=0; i < childeren.length; i++)
+            {
+                childeren[i].style.color = selectedColor;
+                childeren[i].style.fontSize = selectedFontSize;
+                childeren[i].style.backgroundColor = selectedBackColor;
+            }   
+
+            if(e.tagName == "BODY")
+            {
+                e.classList.remove(e.className);
+            }        
+        }
+
+        var reloadButton = document.createElement("button");
+        reloadButton.setAttribute("id", "selectorButton");
+        reloadButton.textContent = "Reset";
+        reloadButton.onclick = function() {location.reload()};
+        menu.appendChild(reloadButton);
     }
-
-    var reloadButton = document.createElement("button");
-    reloadButton.setAttribute("id", "selectorButton");
-    reloadButton.textContent = "Reset";
-    reloadButton.onclick = function() {location.reload()};
-    menu.appendChild(reloadButton);
 
 }
